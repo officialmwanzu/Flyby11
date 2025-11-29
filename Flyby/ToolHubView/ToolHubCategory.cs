3682218427d2869c35255a4b67d315275ead3d05
Collins Mwanzu <104299224+officialmwanzu@users.noreply.github.com>
2025-11-29 17:28:10 +0300

    Create ToolHubCategory.cs

---
+++ Flyby/ToolHubView/ToolHubCategory.cs
@@ -0,0 +1,15 @@
+namespace Flyoobe.ToolHub
+{
+    /// <summary>
+    /// Defines logical groups for setup tools.
+    /// Used to display filtered tools in the ToolHubControlView.
+    /// </summary>
+    public enum ToolHubCategory
+    {
+        All,
+        Tool,
+        Pre,
+        Mid,
+        Post
+    }
+}
