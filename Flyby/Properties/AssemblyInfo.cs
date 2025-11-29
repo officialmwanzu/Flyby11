8f1b3e170d2d4e270d3152bf827428017e6daffe
Collins Mwanzu <104299224+officialmwanzu@users.noreply.github.com>
2025-11-29 17:20:46 +0300

    Create AssemblyInfo.cs

---
+++ Flyby/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("FlyOOBE")]
+[assembly: AssemblyDescription("FlyOOBE")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("A Belim app creation")]
+[assembly: AssemblyProduct("FlyOOBE")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("Builtbybel")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("50ca0d4d-45ad-4e03-bfc5-64fff4cda36c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("2.2.812")]
+[assembly: AssemblyFileVersion("2.2.812")]
