c6b46eab066763b476c929232232506d1bc64838
Collins Mwanzu <104299224+officialmwanzu@users.noreply.github.com>
2025-11-29 15:59:55 +0300

    Create Program.cs

---
+++ Flyby11-deprecated/Flyby11/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flyby11
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
