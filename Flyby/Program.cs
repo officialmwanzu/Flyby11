275c43f7bb03f3295d630e071ea9b2b726ee4b07
Collins Mwanzu <104299224+officialmwanzu@users.noreply.github.com>
2025-11-30 22:08:40 +0300

    Create Program.cs

---
+++ Flyby/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Flyoobe
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+        /// <summary>
+        /// Retrieves the version of the app
+        /// </summary>
+        /// <returns>The application version in the format "major.minor.build"</returns>
+        public static string GetAppVersion()
+        {
+            // Get the version of the current executing assembly
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+
+            // Return the version in the format "major.minor.build"
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+    }
+}
